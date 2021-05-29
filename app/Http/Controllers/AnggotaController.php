a12183911cff90bee6923822772f7920bf94f6c3
unknown <putrirahayunii0209@gmail.com>
2021-05-29 14:54:12 +0700

    default

--- app/Http/Controllers/AnggotaController.php
+++ app/Http/Controllers/AnggotaController.php
@@ -10,15 +10,15 @@ use Illuminate\Http\Request;
 class AnggotaController extends Controller{
     public function anggota(){
         $term = "Ade";
-        $anggota = Anggota::all(); //select all
-        $book = Buku::join('kategori','buku.kategori_id','=','kategori.kategori_id')->get(); //join 
-        $like = Anggota::where('anggota_nama','LIKE','%'.$term.'%')->get();
+        $anggota = Anggota::all(); //select
+        $like = Anggota::where('anggota_nama','LIKE','%'.$term.'%')->get(); //select like
+        $book = Buku::join('kategori','buku.kategori_id','=','kategori.kategori_id')->get(); //select join 
         $joinlike = Pinjam::join('buku','buku.buku_id','=','pinjam.buku_id')->join('anggota','anggota.anggota_id','=','pinjam.anggota_id')->where('buku.buku_judul','=','HTML & CSS')->get();
-        //
+        //join dengan like
         return view('anggota',[
             'anggota' => $anggota,
-            'book'=>$book,
             'like' =>$like,
+            'book'=>$book,
             'joinlike' =>$joinlike
         ]);
     }
